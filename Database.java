2ede1b28cf65ddfeaf51b0e90f6d2d632ef7f52a
Winston Carlile <winston.carlile@gmail.com>
2016-02-18 16:57:49 -0600

    Actually Implemented file loading

--- Database.java
+++ Database.java
@@ -13,16 +13,26 @@ public class Database {
 
     public ArrayList<Song> loadFromFile(String filename) throws FileNotFoundException{
 	// Open the file
-	try{}
 	scanner = new Scanner(new File(filename));
+	
 	// Instantiate the song ArrayList
 	ArrayList<Song> songList = new ArrayList<>();
-	
+
+	// Loop over every 5-line song entry in the file
 	while(scanner.hasNextLine()){
-	    scanner.nextLine();
-	    
-	    
+	    String songName = scanner.nextLine(); // Parse the name
+	    String artist = scanner.nextLine(); // Parse the artist
+	    String album = scanner.nextLine(); // Parse the album
+	    // Time is stored as MM:SS
+	    int minutes = scanner.nextInt(); // Parse the minutes
+	    int seconds = scanner.nextInt(); // Parse seconds
+	    seconds+=minutes*60; // convert minutes into seconds and add to seconds
+	    int year = scanner.nextInt();// Parse year YYYY
+
+	    songList.add(new Song(songName, artist, album, seconds, year));
 	}
+	
+	scanner.close(); // release the file
 	return songList;
     }
 }
