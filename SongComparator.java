527891ffe7cccc5f19e7727905c7592f100e5eac
GabrielGrinff <gabegamergrinff@gmail.com>
2016-02-21 15:18:34 -0600

    Main Method updated with new ArrayList containing 2 sample songs and working song search method. New file SongComparator used to sort songList to make search a linear function, we can change things later if we want but see how this might work

---
+++ SongComparator.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class SongComparator 
+    implements Comparator<Song>, java.io.Serializable{
+    @Override
+    public int compare(Song o1, Song o2){
+        String nameOne = o1.getName();
+        String nameTwo = o2.getName();
+        int comparison = nameOne.compareTo(nameTwo);
+        return comparison;
+    }
+}
