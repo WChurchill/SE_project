cda83d2d7f3abcca3badb504cabd66e0dc45e3fb
Winston Carlile <winston.carlile@gmail.com>
2016-03-16 17:04:06 -0500

    Added assertions for SongDB and CustomerDB

--- CustomerDB.java
+++ CustomerDB.java
@@ -1,21 +1,13 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 
-public class SongDB {
-    Scanner scanner = null;
-    
-    public SongDB(){
+
+public class CustomerDB {
+    public ArrayList<Customer> loadFromFile(string){
 	
     }
 
-
     public ArrayList<Song> loadFromFile(String filename) throws FileNotFoundException{
 	// Open the file
-	scanner = new Scanner(new File(filename));
+	scanner = new Scannder(new File(filename));
 	
 	// Instantiate the song ArrayList
 	ArrayList<Song> songList = new ArrayList<>();
@@ -28,9 +20,6 @@ public class SongDB {
 	    // Time is stored as MM:SS
 	    String line = scanner.nextLine();
 	    int splitIndex = line.indexOf(":");
-	    assert(line.length()>2);
-	    assert(splitIndex>0);
-	    assert(splitIndex<line.length()-2);
 	    int minutes = Integer.parseInt(line.substring(0,splitIndex)); // Parse the minutes
 	    int seconds = Integer.parseInt(line.substring(splitIndex+1)); // Parse seconds
 	    seconds+=minutes*60; // convert minutes into seconds and add to seconds
@@ -43,14 +32,16 @@ public class SongDB {
 	scanner.close(); // release the file
 	return songList;
     }
+    
     public void saveToFile(String filename, ArrayList<Song> songList) throws IOException
     {
     	FileWriter save = new FileWriter(filename);
     	// Loop through the list and print each element to file
     	for(int i =0; i<songList.size(); i++)
-    	{
+	    {
     		save.write(songList.get(i).toString());
-    	}
+	    }
     	save.close();//close the file
     }
+
 }
