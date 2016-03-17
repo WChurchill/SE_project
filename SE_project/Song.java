083869c06b60ad113428dd5f2725ad90fca4995d
GabrielGrinff <gabegamergrinff@gmail.com>
2016-03-16 20:15:46 -0500

    Main now takes songList correctly from database, also album and artist lists are all constructed from SongDB

---
+++ SE_project/Song.java
@@ -0,0 +1,111 @@
+package SE_project;
+
+import java.util.*;
+
+public class Song {
+    private String songName;
+    private String artist;
+    private String album;
+    private int seconds;
+    private int year;
+
+    public Song(String song, String artist, String album, int seconds, int year){
+	this.songName = song;
+	this.artist = artist;
+	this.album = album;
+	this.seconds = seconds;
+	this.year = year;
+    }
+    
+    // Default Constructor
+    public Song(){
+	songName = "<unknown>";
+	artist = "<unknown>";
+	album = "<unknown>";
+	seconds = -1;
+	year = -1;
+    }
+    
+    // Accessors
+    public String getName() {
+	return songName;
+    }
+
+    public String getArtist() {
+	return artist;
+    }
+
+    public String getAlbum() {
+	return album;
+    }
+
+    public int getSeconds() {
+	return seconds;
+    }
+
+    public int getYear() {
+	return year;
+    }
+
+    // Mutators
+    public void setName(String name) {
+	songName = name;
+    }
+
+    public void setArtist(String artist) {
+	this.artist = artist;
+    }
+
+    public void setAlbum(String album) {
+	this.album = album;
+    }
+
+    public void setSeconds(int seconds) {
+	this.seconds = seconds;
+    }
+
+    public void setYear(int year) {
+	this.year = year;
+    }
+
+    // Returns a string representation of a song in human readable format
+    // with labeled attributes
+    public String toLabeledString(){
+	String songString;
+    	String duration;
+    	songString=  " Title:\t"+songName+"\n";
+    	songString+= "Artist:\t"+artist+"\n";
+    	songString+= " Album:\t"+album+"\n";
+    	duration= Integer.toString(seconds/60)+':';
+    	if((seconds%60)<10)// convert from seconds to MM:SS
+	    duration+='0'+Integer.toString(seconds%60); 
+    	else
+	    duration+=Integer.toString(seconds%60); 
+    	songString+= "Length:\t"+duration+"\n";
+    	songString+= "  Year:\t"+year;
+    	return songString;
+    }
+
+    
+    //toString override
+    public String toString()
+    {
+    	String songString;
+    	String duration;
+    	songString= songName+"\r\n";
+    	songString+= artist+"\r\n";
+    	songString+= album+"\r\n";
+    	duration= Integer.toString(seconds/60)+':';
+    	if((seconds%60)<10)// convert from seconds to MM:SS
+    		duration+='0'+Integer.toString(seconds%60); 
+    	else
+    		duration+=Integer.toString(seconds%60); 
+    	songString+= duration+"\r\n";
+    	songString+= year+"\r\n";
+    	return songString;
+    }
+    
+    public int compareTo(Song o){
+        return this.songName.compareTo(o.songName);
+    }
+}
