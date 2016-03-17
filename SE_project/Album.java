f92e34c1c324536dac8c0bdeb3b2f25f7e78c501
GabrielGrinff <gabegamergrinff@gmail.com>
2016-03-16 20:01:15 -0500

    Album and artist now have equal methods, full constructors, etc.

---
+++ SE_project/Album.java
@@ -0,0 +1,51 @@
+package SE_project;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gabriel
+ */
+import java.util.*;
+
+public class Album {
+    private String name;
+    private String artist;
+    private int year;
+    private ArrayList <Song> Songs = new ArrayList<Song>();
+    
+    public Album(String name, int year){
+        this.name = name;
+        this.year = year;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public String getArtist(){
+        return this.artist;
+    }
+    
+    public void addSong(Song addSong){
+        Songs.add(addSong);
+    }
+    
+    @Override
+    public String toString(){
+        return ("\nName: " + this.name + "\nYear: " + this.year);
+    }
+    
+    @Override
+    public boolean equals(Object o){
+        if(o instanceof Album){
+            Album toCompare = (Album) o;
+            return this.name.equals(toCompare.name);
+        }
+    return false;
+    }
+}
