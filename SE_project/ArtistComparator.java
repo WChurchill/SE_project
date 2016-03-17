4aa59405b937495616746b024f4035526f90af7f
GabrielGrinff <gabegamergrinff@gmail.com>
2016-03-16 23:29:56 -0500

    Now you can search all correctly, as the system will remove all ending whitespaces which turned out to be saved in the txt file and caused headache, but fixed!

---
+++ SE_project/ArtistComparator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SE_project;
+
+import java.util.*;
+/**
+ *
+ * @author Gabriel
+ */
+public class ArtistComparator 
+    implements Comparator<Artist>, java.io.Serializable{
+    @Override
+    public int compare(Artist o1, Artist o2){
+        String nameOne = o1.getName();
+        String nameTwo = o2.getName();
+        int comparison = nameOne.compareTo(nameTwo);
+        return comparison;
+    }
+}
