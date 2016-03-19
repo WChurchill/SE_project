d5a2188e86a55c5bd5408169e5a12d6e6dab3b61
GabrielGrinff <gabegamergrinff@gmail.com>
2016-03-19 01:48:32 -0500

    Formattting

---
+++ SE_project/AlbumTimeComparator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SE_project;
+
+import java.util.*;
+/**
+ *
+ * @author Gabriel
+ */
+public class AlbumTimeComparator 
+    implements Comparator<Album>, java.io.Serializable{
+    @Override
+    public int compare(Album o1, Album o2){
+        int yearOne = o1.getYear();
+        int yearTwo = o2.getYear();
+        int comparison = yearOne - yearTwo;
+        return comparison;
+    }
+}
